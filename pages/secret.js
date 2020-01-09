0ad8dea2da7205b648d68982fa95fba2227dcae9
Isaiah Francois <francoisisaiaht@yahoo.com>
2020-01-09 11:26:53 -0500

    Created secret page that can only be viewed if user is logged in

---
+++ pages/secret.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import BaseLayout from '../components/layouts/BaseLayout';
+import BasePage from '../components/layouts/BasePage';
+
+class Secret extends React.Component {
+    renderSecretPage() {
+        const { isAuthenticated } = this.props.auth;
+
+        if (isAuthenticated) {
+            return (
+                <BaseLayout {...this.props.auth}>
+                    <BasePage>
+                        <h1>I am Secret Page</h1>
+                        <p>Secret Content gere</p>
+                    </BasePage>
+                </BaseLayout>
+            );
+        } else {
+            return (
+                <BaseLayout {...this.props.auth}>
+                    <BasePage>
+                        <h1>
+                            You are not authenticated. Please Login to access
+                            this page
+                        </h1>
+                    </BasePage>
+                </BaseLayout>
+            );
+        }
+    }
+
+    render() {
+        return this.renderSecretPage();
+    }
+}
+
+export default Secret;
