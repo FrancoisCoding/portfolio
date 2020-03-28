ee1e94b032fed392e753acc2698796287e576d01
isaiah francois <francoisisaiaht@yahoo.com>
2020-03-28 14:01:55 -0400

    Updated Header component to PureComponent

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,89 +1,87 @@
-import React from 'react';
-import ActiveLink from '../ActiveLink';
+import React, { PureComponent } from "react";
+import ActiveLink from "../ActiveLink";
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem
-} from 'reactstrap';
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem
+} from "reactstrap";
 
 const BsNavLink = props => {
-    const { route, title } = props;
-    const className = props.className || '';
+  const { route, title } = props;
+  const className = props.className || "";
 
-    return (
-        <ActiveLink activeClassName='active' route={route}>
-            <a className={`nav-link port-navbar-link ${className}`}>
-                {' '}
-                {title}{' '}
-            </a>
-        </ActiveLink>
-    );
+  return (
+    <ActiveLink activeClassName="active" route={route}>
+      <a className={`nav-link port-navbar-link ${className}`}> {title} </a>
+    </ActiveLink>
+  );
 };
 
-export default class Header extends React.Component {
-    constructor(props) {
-        super(props);
+export default class Header extends PureComponent {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            isOpen: false,
-            dropdownOpen: false
-        };
+    this.state = {
+      isOpen: false,
+      dropdownOpen: false
+    };
 
-        this.toggle = this.toggle.bind(this);
-        this.toggleDropdown = this.toggleDropdown.bind(this);
-    }
+    this.toggle = this.toggle.bind(this);
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+  }
 
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
 
-    toggleDropdown() {
-        this.setState({
-            dropdownOpen: !this.state.dropdownOpen
-        });
-    }
+  toggleDropdown() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
 
-    render() {
-        const { className } = this.props;
-        const { isOpen } = this.state;
+  render() {
+    console.log("Header");
+    const { className } = this.props;
+    const { isOpen } = this.state;
 
-        const menuOpenClass = isOpen ? 'menu-open' : 'menu-close';
-        return (
-            <div>
-                <Navbar
-                    className={`port-navbar port-nav-base absolute ${className} ${menuOpenClass}`}
-                    color='transparent'
-                    dark
-                    expand='md'
-                >
-                    <NavbarBrand className='port-navbar-brand' href='/'>
-                        Isaiah Francois
-                    </NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className='ml-auto' navbar>
-                            <NavItem className='port-navbar-item'>
-                                <BsNavLink route='/' title='Home' />
-                            </NavItem>
-                            <NavItem className='port-navbar-item'>
-                                <BsNavLink route='/about' title='About' />
-                            </NavItem>
-                            <NavItem className='port-navbar-item'>
-                                <BsNavLink route='/projects' title='Projects' />
-                            </NavItem>
+    const menuOpenClass = isOpen ? "menu-open" : "menu-close";
+    return (
+      <div>
+        <Navbar
+          className={`port-navbar port-nav-base absolute ${className} ${menuOpenClass}`}
+          color="transparent"
+          dark
+          expand="md"
+        >
+          <NavbarBrand className="port-navbar-brand" href="/">
+            Isaiah Francois
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem className="port-navbar-item">
+                <BsNavLink route="/" title="Home" />
+              </NavItem>
+              <NavItem className="port-navbar-item">
+                <BsNavLink route="/about" title="About" />
+              </NavItem>
+              <NavItem className="port-navbar-item">
+                <BsNavLink route="/projects" title="Projects" />
+              </NavItem>
 
-                            <NavItem className='port-navbar-item'>
-                                <BsNavLink route='/cv' title='Cv' />
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-            </div>
-        );
-    }
+              <NavItem className="port-navbar-item">
+                <BsNavLink route="/cv" title="Cv" />
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
