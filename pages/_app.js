f99b4a7d30bf4e027124c05406b74f9380bea059
Isaiah <francoisisaiaht@yahoo.com>
2022-07-21 00:46:26 -0400

    Fixed sass issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import App, { Container } from 'next/app';
-import { ToastContainer } from 'react-toastify';
-import Fonts from '../helpers/Fonts';
+import React from "react";
+import App, { Container } from "next/app";
+import { ToastContainer } from "react-toastify";
+import Fonts from "../helpers/Fonts";
 
 // Stylings
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/main.scss';
-import 'react-toastify/dist/ReactToastify.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/main.scss";
+import "react-toastify/dist/ReactToastify.css";
 
 export default class MyApp extends App {
-    static async getInitialProps({ Component, ctx }) {
-        let pageProps = {};
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
 
-        if (Component.getInitialProps) {
-            pageProps = await Component.getInitialProps(ctx);
-        }
-
-        return { pageProps };
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
     }
 
-    componentDidMount() {
-        // Fonts();
-    }
+    return { pageProps };
+  }
 
-    render() {
-        const { Component, pageProps } = this.props;
+  componentDidMount() {
+    // Fonts();
+  }
 
-        return (
-            <Container>
-                <ToastContainer />
-                <Component {...pageProps} />
-            </Container>
-        );
-    }
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <ToastContainer />
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
 }
